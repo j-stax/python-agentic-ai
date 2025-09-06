48cab40df78ef474105107eba9ce5b28bcb790f8
j-stax <ahnjames01@gmail.com>
2025-09-06 09:57:13 -0400

    Dynamic prompt input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,10 +10,13 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    # print("Hello from python-agentic-ai!")
+    if (len(sys.argv) < 2):
+        print("Usage: uv run main.py <prompt>")
+        sys.exit(1)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=sys.argv[1]
     )
     print(response.text)
     print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')

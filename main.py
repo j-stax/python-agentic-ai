aefa87b013e2144a91864a7467d14600b72b79ff
j-stax <ahnjames01@gmail.com>
2025-09-07 18:40:21 -0400

    Optional --verbose flag in user prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
@@ -13,14 +14,25 @@ def main():
     if (len(sys.argv) < 2):
         print("Usage: uv run main.py <prompt>")
         sys.exit(1)
+    
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=sys.argv[1]
+        contents=messages,
     )
-    print(response.text)
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    # print(response.text)
+    # print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    # print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
+    if '--verbose' in sys.argv:
+        print(f'User prompt: {user_prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
 
 
 if __name__ == "__main__":

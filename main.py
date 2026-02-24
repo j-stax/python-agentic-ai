347237c5650cd9393da91651558b02c1954465fb
j-stax <ahnjames01@gmail.com>
2026-02-23 23:58:45 -0500

    Refactor/clean-up main.py

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 import os
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
 from prompts import system_prompt
 from call_function import available_functions, call_function
 
-load_dotenv()
-
-api_key = os.environ.get("GEMINI_API_KEY")
-model_name= "gemini-2.5-flash"
-client = genai.Client(api_key=api_key)
+MODEL_NAME="gemini-2.5-flash"
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    user_prompt = args.user_prompt
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f'User prompt: {args.user_prompt}\n')
+
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model=model_name, 
+        model=MODEL_NAME, 
         contents=messages,
         config=types.GenerateContentConfig(
             tools=[available_functions],
             system_instruction=system_prompt
         ),
     )
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
 
     if response.usage_metadata == None:
         raise RuntimeError("Error accessing usage_metadata property - possible failed API request.")
     
-    if args.verbose:
-        print(f'User prompt: {user_prompt}')
-        print(f'Prompt tokens: {prompt_tokens}')
-        print(f'Response tokens: {response_tokens}')
-    
+    if verbose:
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
     if not response.function_calls:
-        print(f'Response:\n {response.text}')
+        print(f'Response:\n{response.text}')
         return
 
     function_results = []
@@ -53,10 +60,9 @@ def main():
             or not function_call_result.parts[0].function_response.response
         ):
             raise RuntimeError(f'Error: Empty function response for {function_call.name}')
-        if args.verbose:
+        if verbose:
             print(f"-> {function_call_result.parts[0].function_response.response}")
         function_results.append(function_call_result.parts[0])
-
    
 if __name__ == "__main__":
     main()
